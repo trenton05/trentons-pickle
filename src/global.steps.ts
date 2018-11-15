759a01b08fee43674f6a6156dbc22f21da89932b
trenton05 <tralbrecht@gmail.com>
2018-11-15 10:37:13 -0800

    Create global.steps.ts

---
+++ src/global.steps.ts
@@ -0,0 +1,89 @@
+import { binding, given, then, when } from 'cucumber-tsflow';
+import { mapping } from './mapping';
+import { browser, ExpectedConditions } from 'protractor';
+import * as Chance from 'chance';
+import { BrowserUtil } from './browser-util';
+
+const chance = new Chance();
+
+@binding()
+export default class GlobalSteps {
+
+  @mapping(/^random (.*)$/i)
+  static async randomMapping(type: string): Promise<string> {
+    return chance[type]();
+  }
+
+  @when(/^I navigate to (.*)$/i)
+  public async navigate(pageMatch: string): Promise<void> {
+    const page: string = await mapping(pageMatch);
+    await browser.get(page);
+  }
+
+  @then(/^(.*) is in the url/i)
+  public async contains(pageMatch: string): Promise<void> {
+    const page: string = await mapping(pageMatch);
+    await browser.wait(ExpectedConditions.urlContains(page));
+  }
+
+  @then(/^(.*) is the url/i)
+  public async loaded(pageMatch: string): Promise<void> {
+    let page: string = await mapping(pageMatch);
+    page = browser.baseUrl + page;
+    await browser.wait(ExpectedConditions.urlIs(page));
+  }
+
+  @given(/^Random (.*) as (.*)$/i)
+  public randomString(type: string, name: string): void {
+    if (type === 'id') {
+      return mapping(name, IdUtil.randomId());
+    } else {
+      return mapping(name, chance[type]());
+    }
+  }
+
+  @given(/^The text in (.*) as (.*)$/i)
+  public async assignText(type: string, name: string): Promise<void> {
+    await BrowserUtil.safeWait(async () => {
+      const elt: any = await mapping(type);
+      const finder = BrowserUtil.finder(elt);
+      const text: string = await finder.getText();
+      mapping(name, text);
+    });
+  }
+
+  @given(/^The value in (.*) as (.*)$/i)
+  public async assignValue(type: string, name: string): Promise<void> {
+    await BrowserUtil.safeWait(async () => {
+      const elt: any = await mapping(type);
+      const finder = BrowserUtil.finder(elt);
+      const text: string = await finder.getAttribute('value');
+      mapping(name, text);
+    });
+  }
+
+  @given(/^Local storage is empty$/i)
+  public async emptyLocalStorage(): Promise<void> {
+    await browser.executeScript('window.localStorage.clear();');
+  }
+
+  @given(/^Session storage is empty$/i)
+  public async emptySessionStorage(): Promise<void> {
+    await browser.executeScript('window.sessionStorage.clear();');
+  }
+
+  @then(/I log the browser console called '(.*)'/)
+  public async logConsole(name: string): Promise<void> {
+    await captureLogs(name);
+  }
+
+  @given(/^wait for Angular is (on|off)$/i)
+  public async setWaitForAngular(enabledStr: string): Promise<any> {
+    browser.waitForAngularEnabled(enabledStr === 'on');
+  }
+
+  @when(/^I (?:reload|refresh) the page$/i)
+  public async reloadPage(): Promise<void> {
+    await browser.driver.navigate().refresh();
+  }
+}
